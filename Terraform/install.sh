6d08a8b2cea0fbd3a39df1516fb2060eaef6722a
TheTop <noppakron.son@dome.tu.ac.th>
2026-04-03 20:39:08 +0700

    chore: Add Jenkins and Sonarqube installation script

--- Terraform/install.sh
+++ Terraform/install.sh
@@ -15,3 +15,35 @@ apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin do
 usermod -aG docker ubuntu
 systemctl enable docker
 systemctl start docker
+
+chmod 777 /var/run/docker.sock 
+
+# Install Jenkins
+echo "🚀 Installing Jenkins..."
+wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+apt-get update -y
+apt-get install -y jenkins
+
+systemctl enable jenkins
+systemctl start jenkins
+
+# เอา user jenkins เข้า group docker จะได้สั่ง build image ได้
+usermod -aG docker jenkins
+
+# Install SonarQube
+echo "🔍 Starting SonarQube Container..."
+# รอให้ Docker service สตาร์ทเสร็จสมบูรณ์ก่อนสั่งรัน Container
+sleep 5 
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+
+# Finish & Summary
+echo "========================================================"
+echo "✅ Setup Complete!"
+echo " Jenkins: http://44.212.44.228:8080"
+echo " SonarQube: http://44.212.44.228:9000"
+echo "========================================================"
+echo "🔐 Your Jenkins Initial Admin Password is:"
+cat /var/lib/jenkins/secrets/initialAdminPassword
+echo "========================================================"
